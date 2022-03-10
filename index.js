148ff8e2aec422fde646c5b680c96c29097651ca
shivamofficial <91952992+shivamofficial@users.noreply.github.com>
2022-03-10 21:34:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+//using  array iteral
+let aar=[1,2,3,4,5]; //empty aaray
+console.log("arr", arr);
+//using constructor function
+let anotherarr =new array [1,2,3,4,5,6,6];
+console.log("anotherarr", anotherarr);
+console.log(arr[3]);
+let value =["a","e","i","o","u"];
+console.log(value.length);
+value[10]=12;
+console.log(value[6]);
+
+/*
+function countString (arr){
+    let count = 0;
+    let tempArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (typeof(element)==="string") count++;
+        // if (typeof(element)==="string") tempArr.push(element)
+    }
+    return tempArr;
+}
+
+const result = countString(["a", "b", "c", 1, 3, 5]);
+console.log(result);
+*/
+[ 'apple', 'banana', 'orange' ]
+orange
+[ 'apple', 'banana' ]
+3
+4
+[ 'apple', 'banana', 'strawberry', 'melon' ]
+/*
+[ 'apple', 'banana', 'orange' ]
+orange
+[ 'apple', 'banana' ]
+3
+4
+[ 'apple', 'banana', 'strawberry', 'melon' ]
+*/
